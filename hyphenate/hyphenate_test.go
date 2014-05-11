0bcbfe8e75f8febceb6c2bf145f558d9dc1fe1a8
aditya <dev@chimeracoder.net>
2014-05-11 16:33:52 -0400

    Add unit tests for hyphenation

---
+++ hyphenate/hyphenate_test.go
@@ -0,0 +1,44 @@
+package hyphenate
+
+import (
+    "testing"
+)
+
+
+func Test_CMUDict(t *testing.T) {
+    err := loadCMUCorpus()
+    if err != nil{
+        t.Error(err)
+    }
+
+    const expectedLength = 133265
+    if len(cmuCorpus) != expectedLength {
+        t.Errorf("Expected %d words in corpus and found %d", expectedLength, len(cmuCorpus))
+    }
+}
+
+func Test_CMUSyllables(t *testing.T){
+    err := loadCMUCorpus()
+    if err != nil{
+        t.Error(err)
+    }
+
+    TestWords := map[string]int{
+        "Zombie" : 2,
+        "antibiotic" : 5,
+        "RECORDED" : 3,
+        "recaptured" : 3,
+        "blame" : 1,
+        "discussion" : 3,
+        "whims" : 1,
+        "tilting" : 2,
+        "gong" : 1,
+    }
+
+    for word, expected := range TestWords {
+        if syllables := cmuCorpus.Syllables(word); syllables != expected {
+            t.Errorf("expected %d syllables for \"%s\" and received %d", expected, word, syllables)
+        }
+    }
+
+}
