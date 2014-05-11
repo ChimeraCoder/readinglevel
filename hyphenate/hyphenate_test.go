fc3d2f3853a1cef4e69bb1bdba370136993c2d32
aditya <dev@chimeracoder.net>
2014-05-11 16:54:42 -0400

    Run gofmt on all files

--- hyphenate/hyphenate_test.go
+++ hyphenate/hyphenate_test.go
@@ -1,44 +1,43 @@
 package hyphenate
 
 import (
-    "testing"
+	"testing"
 )
 
-
 func Test_CMUDict(t *testing.T) {
-    err := loadCMUCorpus()
-    if err != nil{
-        t.Error(err)
-    }
+	err := loadCMUCorpus()
+	if err != nil {
+		t.Error(err)
+	}
 
-    const expectedLength = 133265
-    if cmuCorpusCached.Words() != expectedLength {
-        t.Errorf("Expected %d words in corpus and found %d", expectedLength, cmuCorpusCached.Words())
-    }
+	const expectedLength = 133265
+	if cmuCorpusCached.Words() != expectedLength {
+		t.Errorf("Expected %d words in corpus and found %d", expectedLength, cmuCorpusCached.Words())
+	}
 }
 
-func Test_CMUSyllables(t *testing.T){
-    err := loadCMUCorpus()
-    if err != nil{
-        t.Error(err)
-    }
+func Test_CMUSyllables(t *testing.T) {
+	err := loadCMUCorpus()
+	if err != nil {
+		t.Error(err)
+	}
 
-    TestWords := map[string]int{
-        "Zombie" : 2,
-        "antibiotic" : 5,
-        "RECORDED" : 3,
-        "recaptured" : 3,
-        "blame" : 1,
-        "discussion" : 3,
-        "whims" : 1,
-        "tilting" : 2,
-        "gong" : 1,
-    }
+	TestWords := map[string]int{
+		"Zombie":     2,
+		"antibiotic": 5,
+		"RECORDED":   3,
+		"recaptured": 3,
+		"blame":      1,
+		"discussion": 3,
+		"whims":      1,
+		"tilting":    2,
+		"gong":       1,
+	}
 
-    for word, expected := range TestWords {
-        if syllables := cmuCorpusCached.Syllables(word); syllables != expected {
-            t.Errorf("expected %d syllables for \"%s\" and received %d", expected, word, syllables)
-        }
-    }
+	for word, expected := range TestWords {
+		if syllables := cmuCorpusCached.Syllables(word); syllables != expected {
+			t.Errorf("expected %d syllables for \"%s\" and received %d", expected, word, syllables)
+		}
+	}
 
 }
